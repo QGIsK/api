87d4d3c40a001fbbd5230daffaa8838c712a3df4
Demian <devaccdemiann@gmail.com>
2021-11-21 21:39:44 +0100

    create: docs with nuxt/content

---
+++ docs/nuxt.config.js
@@ -0,0 +1,17 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  head: {
+    script: [
+      {
+        'async': true,
+        'defer': true,
+        'data-domain': 'api.demiann.dev',
+        'src': 'https://analytics.demiann.dev/js/plausible.js',
+      },
+    ],
+  },
+  docs: {
+    primaryColor: '#E24F55',
+  },
+})
