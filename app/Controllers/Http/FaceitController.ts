6b062a8e06340fee31c78229f21bf8a9a34e1ee9
Demian <devaccdemiann@gmail.com>
2021-11-21 21:34:53 +0100

    chore: cleanup

--- app/Controllers/Http/FaceitController.ts
+++ app/Controllers/Http/FaceitController.ts
@@ -2,7 +2,6 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Env from '@ioc:Adonis/Core/Env'
 
 import FaceitAPI from '@cstools-app/faceit-wrapper'
-import fetch from 'node-fetch'
 
 // Initiate the client
 const FaceitClient = new FaceitAPI(Env.get('FACEIT_KEY'))
@@ -15,14 +14,20 @@ export default class FaceitController {
       ? await FaceitClient.players.get({ nickname: query.username })
       : await FaceitClient.players.show({ player_id: query.id })
 
+    if (profile.errors) return response.notFound(profile)
+
     const gameIndex = query.game ? query.game : 'csgo'
+    const skillLevel = profile.games[gameIndex].skill_level
+    const elo = profile.games[gameIndex].faceit_elo
+
+    const icon = `https://${Env.get('CDN_BASE')}/${Env.get('FACEIT_ICON_PATH')}/level${
+      profile.games[gameIndex].skill_level
+    }.svg`
 
     return response.send({
-      skill_level: profile.games[gameIndex].skill_level,
-      elo: profile.games[gameIndex].faceit_elo,
-      icon: `https://${Env.get('CDN_BASE')}/${Env.get('FACEIT_ICON_PATH')}/level${
-        profile.games[gameIndex].skill_level
-      }.svg`,
+      skillLevel,
+      elo,
+      icon,
     })
   }
 }
